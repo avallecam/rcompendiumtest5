cb44dea8f9c663364ea32cef2637ba0628c871c8
Andree Valle <avallecam@gmail.com>
2023-08-19 20:08:00 +0100

    list all scripts in sequential order

--- make.R
+++ make.R
@@ -29,3 +29,7 @@ devtools::load_all(here::here())
 
 # List all R scripts in a sequential order and using the following form:
 # source(here::here("analyses", "script_X.R"))
+
+source(here::here("analyses", "01-read_data.R"))
+source(here::here("analyses", "02-plot_incidence.R"))
+source(here::here("analyses", "03-estimate_cfr.R"))
