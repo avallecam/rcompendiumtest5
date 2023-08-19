4ad8cbf39791abba077c26ad665e595a64920752
Andree Valle <avallecam@gmail.com>
2023-08-19 19:00:40 +0100

    fix factor creation and clean data

--- analyses/01-read_data.R
+++ analyses/01-read_data.R
@@ -1,21 +1,21 @@
 
-#' Install a package
-# pak::pak("readxl")
-# pak::pak("tidyverse")
+# Package info
+#' {readxl} https://readxl.tidyverse.org/
+#' {tidyverse} https://www.tidyverse.org/
 
-#' Load packages
+# Load packages
 library(readxl)
 library(tidyverse)
 
-#' Read raw data
+# Read raw data
 dat <- readxl::read_xlsx("data/raw-data/linelist_20140701.xlsx")
 
-#' Clean raw data
+# Clean raw data
 dat_clean <- dat %>% 
   select(case_id,date_of_onset,date_of_outcome,outcome) %>% 
   mutate(across(.cols = c(date_of_onset,date_of_outcome),
                 .fns = as.Date)) %>% 
-  mutate(outcome = as.factor(outcome))
+  mutate(outcome = fct(outcome,level = c("Death","Recover"),na = "NA"))
 
 # Write clean data
 dat_clean %>% 
